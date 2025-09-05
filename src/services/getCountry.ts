bdce2535089e5313265c1537454562d3593e6087
ahmedhamdy223 <ahmedhamdy@quickly.codes>
2025-09-05 07:19:10 +0300

    Update getCountry.ts

--- src/services/getCountry.ts
+++ src/services/getCountry.ts
@@ -1,15 +1,1 @@
-import axios from "axios";
-import type { CountryCityResponse } from "../types/interfaces";
 
-const getCountry = async () => {
-  try {
-    const response = await axios.get(
-      "https://countriesnow.space/api/v0.1/countries"
-    );
-    return response.data;
-  } catch {
-    throw new Error();
-  }
-};
-
-export default getCountry;
